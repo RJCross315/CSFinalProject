79cda94e5e1cb948024890c040bbc46fe0fbb632
RJCross315 <rjc35@students.calvin.edu>
2020-12-02 15:47:06 -0500

    basic request and routing code

--- finalProj/src/app/app-routing.module.ts
+++ finalProj/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PrayerBoardComponent } from './pages/prayer-board/prayer-board.component';
+import { PrayerRequestComponent } from './pages/prayer-request/prayer-request.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'request', component: PrayerRequestComponent },
+  { path: 'board', component: PrayerBoardComponent }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
